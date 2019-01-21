410a02a77eb4959304d8611a63eb333a577a557f
Chyld Medford <chyld.medford@gmail.com>
2019-01-21 15:16:34 -0800

    http append

--- api/test/test_flask.py
+++ api/test/test_flask.py
@@ -1,4 +1,11 @@
+import pytest
+
 import requests
+
+
+@pytest.fixture
+def clear():
+    requests.get('http://localhost:5000/clear')
 
 
 def test_status():
@@ -11,3 +18,10 @@ def test_clear():
     response = requests.get('http://localhost:5000/clear')
     response = response.json()
     assert response['status'] == 'cleared'
+
+
+def test_append(clear):
+    requests.get('http://localhost:5000/append/3.14')
+    response = requests.get('http://localhost:5000/append/2.71')
+    response = response.json()
+    assert response['list'] == '3.14 -> 2.71'
