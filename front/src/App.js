04cb1bdf5eaa2542ad5d56ef60918d43bb338bfb
Chyld Medford <chyld.medford@gmail.com>
2019-01-22 20:02:43 -0800

    react clear prepend

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.append = this.append.bind(this);
+    this.prepend = this.prepend.bind(this);
+    this.clear = this.clear.bind(this);
   }
 
   async append() {
@@ -15,11 +17,28 @@ class App extends Component {
     console.log(data);
   }
 
+  async prepend() {
+    const val = parseInt(this.in_val.value);
+    const url = `http://localhost:5000/prepend/${val}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  async clear() {
+    const url = `http://localhost:5000/clear`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className="App">
         <input ref={input => (this.in_val = input)} type="text" />
         <button onClick={this.append}>Append</button>
+        <button onClick={this.prepend}>Prepend</button>
+        <button onClick={this.clear}>Clear</button>
       </div>
     );
   }
